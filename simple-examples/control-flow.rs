4e73ada0170ba469264aaf79daf2ff47b574d0df
Ariel Carvajal Boyd <arie.cbpro@gmail.com>
2022-11-11 22:32:24 +0000

    Add examples

--- simple-examples/control-flow.rs
+++ simple-examples/control-flow.rs
@@ -66,4 +66,37 @@ fn main(){
     for el in 1..6 {
         println!("{}", el * el);
     }
+
+    // match contol flow
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+        // --snip--
+    }
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+    
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
+    let val = value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    println!("{}", val);
 }
+
---
+++ simple-examples/enum.rs
@@ -0,0 +1,62 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+#[derive(Debug)]
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum IpAddr3 {
+    V4(u8, u8, u8, u8)
+}
+fn route(_ip_kind: IpAddrKind) {}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main (){
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    println!("{:?} {:?}", home, loopback);
+
+    let home = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr2::V6(String::from("::1"));
+    println!("{:?} {:?}", home, loopback);
+
+    let home = IpAddr3::V4(127, 0, 0, 1);
+    println!("{:?}", home);
+
+    // valores para Option<T>
+    let _some_number = Some(5);
+    let _some_char = Some('e');
+
+    let _absent_number: Option<i32> = None;
+}
