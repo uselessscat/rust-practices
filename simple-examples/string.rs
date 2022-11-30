24d65661caa6531fde27af1bb8bd767f2a1452d5
Ariel Carvajal Boyd <arie.cbpro@gmail.com>
2022-11-30 21:21:44 +0000

    Add examples

---
+++ simple-examples/string.rs
@@ -0,0 +1,32 @@
+fn main(){
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+    // equivalente
+    let s = String::from("initial contents");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("s is {}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+}
