c888d47f766106fc21112cb92379e97e76260e33
Uselessscat <arie.cbpro@gmail.com>
2023-01-19 22:48:52 -0300

    Add examples

---
+++ simple-examples/generics.rs
@@ -0,0 +1,57 @@
+// generics en enums
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+// generics en struct
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let _both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.1, y: 4.1 };
+    let integer_and_float = Point { x: 5, y: 4.2 };
+
+    println!("p.x = {}", integer_and_float.x());
+    println!("p.y = {}", integer_and_float.y());
+    println!("distance = {}", both_float.distance_from_origin());
+
+    let p3 = both_float.mixup(integer_and_float);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+}
+
---
+++ simple-examples/traits.rs
@@ -0,0 +1,102 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+    notify(&article);
+    notify(&tweet);
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// this is the same as above
+pub fn notify2<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// both parameters should implement summary, but can be different types
+pub fn notify3(item1: &impl Summary, item2: &impl Summary) {}
+
+// both parameters should implement summary but both should be the same type
+pub fn notify4<T: Summary>(item1: &T, item2: &T) {}
+
+// if we want to use multiple traits it can be done with
+pub fn notify5(item: &(impl Summary + Display)) {}
+pub fn notify6<T: Summary + Display>(item: &T) {}
+
+// a function can have multiple traits, an alternative syntax is
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {}
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+}
