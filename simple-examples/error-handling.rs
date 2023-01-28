22a93c48c703dc0bd61d36f204b8ea5afd366945
Uselessscat <arie.cbpro@gmail.com>
2023-01-27 23:22:07 -0300

    Add example

--- simple-examples/error-handling.rs
+++ simple-examples/error-handling.rs
@@ -1,3 +1,64 @@
+use std::fs::File;
+use std::io::{self, Read, ErrorKind};
+
 fn main() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+
+    // If the Result value is the Ok variant, unwrap will return the value inside 
+    // the Ok. If the Result is the Err variant, unwrap will call the panic! macro for us.
+    let greeting_file = File::open("hello.txt").unwrap();
+
+    // the expect method lets us also choose the panic! error message.
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
+
     panic!("crash and burn");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn read_username_from_file4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
